46d097282f9bb3be313104433960c1ddb47c01d8
ELISEY-MATISN <MrMatisn@yandex.ru>
2025-11-05 13:42:50 +0300

    Update java.java

--- java.java
+++ java.java
@@ -52,9 +52,10 @@ public class Main {
         g.addEdge(0, 1);
         g.addEdge(0, 2);
         g.addEdge(1, 3);
-        g.addEdge(2, 4);
+        g.addEdge(1, 4);
         
         g.dfs(0);
         g.printTimes();
     }
+
 }
